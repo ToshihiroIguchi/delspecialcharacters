fff944c7e7680aaaba14d627e3bf383c3865ef16
ToshihiroIguchi <toshihiro.iguchi.mail@gmail.com>
2021-03-16 22:33:10 +0900

    add new conv

--- delspecialcharacters.R
+++ delspecialcharacters.R
@@ -1,45 +1,70 @@
-#特殊文字を削除
-#ただし、漢字は直接使用しない
-
-
-#xのベクトルで、hexではじまるunicodeをvecの文字に変換
-conv.hex.chr <- function(x, hex, vec){
-  
-  #https://qiita.com/hoxo_m/items/82ad2493f73d20648b1c
-  #十六進数を数値に変換
-  first.hex.num <- strtoi(hex, base = 16)
-  
-  #数値のベクトルを作成
-  hex.num <- first.hex.num + c(0 : (length(vec) - 1))
-  
-  #16進数で再生
-  hex.vec <- as.character(as.hexmode(hex.num))
-  
-  #unicodeなので先頭に付け足す
-  hex.vec <- paste0("0x", hex.vec)
-  
-  
-  print(hex.vec)
-  
-  #戻り値に初期値を入れておく
-  ret <- x
-  
-  #unicodeで順に変換していく
-  for(i in 1:length(vec)){
-    #https://stackoverflow.com/questions/66639163/convert-from-hexadecimal-to-unicode-in-r-language/66639415#66639415
-    ret <- gsub(pattern = intToUtf8(hex.vec[i]) , replacement = vec[i], x = ret)
-  }
-  
-  #戻り値
-  return(ret)
-  
-}
-
 #caretのformulaで特殊文字を入力すると不具合が生じるので削除する関数
-delspecialcharacters <- function(vec){
+delspecialcharacters <- function(vec, o.num = TRUE){
   
   #vecは置換対象のベクトル
   #先頭に数字が来たら"O(オー)"をつける。
+  
+  #xのベクトルで、hexではじまるunicodeをvecの文字に変換
+  conv.hex.chr <- function(x, hex, vec){
+    
+    #https://qiita.com/hoxo_m/items/82ad2493f73d20648b1c
+    #十六進数を数値に変換
+    first.hex.num <- strtoi(hex, base = 16)
+    
+    #数値のベクトルを作成
+    hex.num <- first.hex.num + c(0 : (length(vec) - 1))
+    
+    #16進数で再生
+    hex.vec <- as.character(as.hexmode(hex.num))
+    
+    #unicodeなので先頭に付け足す
+    hex.vec <- paste0("0x", hex.vec)
+    
+    #戻り値に初期値を入れておく
+    ret <- x
+    
+    #unicodeで順に変換していく
+    for(i in 1:length(vec)){
+      #https://stackoverflow.com/questions/66639163/convert-from-hexadecimal-to-unicode-in-r-language/66639415#66639415
+      ret <- gsub(pattern = intToUtf8(hex.vec[i]) , replacement = vec[i], x = ret)
+    }
+    
+    #戻り値
+    return(ret)
+    
+  }
+  
+  #hexではじまるunicodeをn個の文字に変換
+  make.hex.chr <- function(hex, n = 1){
+    
+    #https://qiita.com/hoxo_m/items/82ad2493f73d20648b1c
+    #十六進数を数値に変換
+    first.hex.num <- strtoi(hex, base = 16)
+    
+    #数値のベクトルを作成
+    hex.num <- first.hex.num + c(0 : (n - 1))
+    
+    #16進数で再生
+    hex.vec <- as.character(as.hexmode(hex.num))
+    
+    #unicodeなので先頭に付け足す
+    hex.vec <- paste0("0x", hex.vec)
+    
+    #戻り値
+    ret <- rep(NA, n)
+    
+    #それぞれ戻り値に変換
+    for(i in 1:length(hex.vec)){
+      #UTF8に変換
+      ret[i] <- intToUtf8(hex.vec[i])
+    }
+    
+    
+    #戻り値
+    return(ret)
+    
+  }
+
   
   #置換用の関数
   gsub2 <- function(x, pattern, replacement = ""){
@@ -61,8 +86,9 @@ delspecialcharacters <- function(vec){
   #全角数字を変換
   ret <- conv.hex.chr(ret, hex = "FF10", c(0:9))
   
-  #丸の中の数字を変換
+  #丸の中の数字を変換(1-20)
   ret <- conv.hex.chr(ret, hex = "2460", c(1:20))
+  
   
   #上付き数字を変換
   #https://unicode-table.com/jp/sets/superscript-and-subscript-letters/
@@ -124,21 +150,36 @@ delspecialcharacters <- function(vec){
   ret <- gsub2(ret, "\u03c8", "Psi")
   ret <- gsub2(ret, "\u03C9", "Omega")
   
+  
+  #特殊文字
+  ret <- gsub2(ret, "\u2103", "C")
+  ret <- gsub2(ret, "\u2116", "No")
+  
+  #ローマ数字を変換
+  ret <- conv.hex.chr(ret, hex = "2160", c(1:12))
+  
+  #カッコつき漢数字を変換
+  ret <- conv.hex.chr(ret, hex = "3220", make.hex.chr("4E00", n = 10))
+  
+  
   #漢字、ひらがな、カタカナ、数字、アルファベット大文字小文字以外を削除
   #https://qiita.com/grrrr/items/0b35b5c1c98eebfa5128
   #https://so-zou.jp/software/tech/programming/tech/regular-expression/meta-character/variable-width-encoding.htm#no1
   #サロゲートペアは含めない
   ret <- gsub2(ret, "[^\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF0-9a-zA-Z\u30FC]", "")
   
-  #先頭に数字がくるベクトル中の位置
-  num.pos <- grep("^[0-9]", ret)
-  
-  #先頭に数字がくる場合、"O"(オー)"をつける。
-  ret[num.pos] <- paste0("O", ret[num.pos])
+  #先頭に数字がくる場合の処理
+  if(o.num){
+
+    #先頭に数字がくるベクトル中の位置
+    num.pos <- grep("^[0-9]", ret)
+    
+    #先頭に数字がくる場合、"O"(オー)"をつける。
+    ret[num.pos] <- paste0("O", ret[num.pos])
+    
+  }
   
   #戻り値
   return(ret)
 }
-
-
 
